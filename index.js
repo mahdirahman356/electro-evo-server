dca84d44f5f0055e7919916525dd98913eeb4bb2
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2024-05-13 05:34:16 +0600

    added jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
@@ -12,6 +13,8 @@ app.use(cors({
   credentials: true
 }))
 app.use(express.json())
+app.use(cookieParser())
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rz0kihv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,7 +30,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+
+
+    const verifyToken = (req, res, next) => {
+      const token = req?.cookies?.token
+      if(!token){
+        return res.status(401).send({message : "unauthorise access"})
+      }
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if(err){
+            return res.status(401).send({message : "unauthorise access"})
+          }
+          req.user = decoded
+          next()
+      })
+
+    }
+    
 
     const database = client.db("queriesDB");
     const queriesCollection = database.collection("queries");
@@ -62,14 +82,19 @@ async function run() {
         ];
       }
 
-      if (email) {
-        query.email = email;
-      }
-
       const options = {};
 
       const result = await queriesCollection.find(query, options).toArray();
       res.send(result);
+    })
+
+    app.get("/queries/email/:email",verifyToken, async (req, res) => {
+      if(req.user.email !== req.params.email){
+        return res.status(403).send({message : "forbidden access"})
+      }
+      query = { email: req.params.email }
+      const result = await queriesCollection.find(query).toArray()
+      res.send(result)
     })
 
     app.get("/queries/:id", async (req, res) => {
@@ -108,7 +133,7 @@ async function run() {
       const queries = req.body
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
-        $set: queries,
+        $set: {recommendationCount: queries.recommendationCount},
       };
       const result = await queriesCollection.updateOne(filter, updateDoc);
       res.send(result)
@@ -133,13 +158,19 @@ async function run() {
       res.send(result)
     })
 
-    app.get("/recommend/myRecommrnd/:email", async (req, res) => {
+    app.get("/recommend/myRecommrnd/:email",verifyToken, async (req, res) => {
+      if(req.user.email !== req.params.email){
+        return res.status(403).send({message : "forbidden access"})
+      }
       query = { recommendationEmail: req.params.email }
       const result = await recommendCollection.find(query).toArray()
       res.send(result)
     })
 
-    app.get("/recommend/RecommendForMe/:email", async (req, res) => {
+    app.get("/recommend/RecommendForMe/:email",verifyToken, async (req, res) => {
+      if(req.user.email !== req.params.email){
+        return res.status(403).send({message : "forbidden access"})
+      }
       query = { email: req.params.email }
       const result = await recommendCollection.find(query).toArray()
       res.send(result)
